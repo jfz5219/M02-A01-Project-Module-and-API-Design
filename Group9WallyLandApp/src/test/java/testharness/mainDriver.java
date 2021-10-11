8d3a8ffa9f9af00748bce9eff4dddba8fe4cfa8c
ejk1220 <ejkennedy137@gmail.com>
2021-10-10 20:13:10 -0400

    added mainDriver changes

--- Group9WallyLandApp/src/test/java/testharness/mainDriver.java
+++ Group9WallyLandApp/src/test/java/testharness/mainDriver.java
@@ -1,14 +1,17 @@
 package testharness;
 package EmployeePortal;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.Comparator;
+import main.java.RealTimeInfo.ParkEvent;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class mainDriver {
     
     @Test (expected=NullPointerException.class)
-    public void getAttractionsTest() {
+        public void getAttractionsTest() {
         String attraction = null;
         ArrayList<String> attractionList = new ArrayList<>();
         attractionList.add("Needs");
@@ -28,11 +31,30 @@ public class mainDriver {
     
     
     
-    /**
-     * This main method is the main driver for our program 
-     * @param args 
-     */
+  static private ParkEvent myEvents;
+private static final String String = null;
+private static final LocalDate LocalDate = null;
+    
     public static void main(String[] args) {
         
+        System.out.println("Running ALL testharness test stubs......");
+
+        ParkEvent app = new ParkEvent(String, LocalDate, LocalDate);
+        eventList();
+                
+        System.out.println("");
+       
+        System.out.println("The End...  finished ALL test stubs for all packages-classes-methods");
     }
+    public static void eventList () {
+            ArrayList< ParkEvent > events = new ArrayList<>();
+            events.add( new ParkEvent( "Christmas Spectuacular" , java.time.LocalDate.of( 2021 , Month.DECEMBER, 17 ) , java.time.LocalDate.of( 2021 , Month.JANUARY, 1 ) ) );
+            events.add( new ParkEvent( "Fright Night" , java.time.LocalDate.of( 2021 , Month.OCTOBER, 17 ) , java.time.LocalDate.of( 2021 , Month.OCTOBER , 31 ) ) );
+            
+            
+            for (ParkEvent event : events) {
+            System.out.println(event.toString());
+
+        }
+}
 }
