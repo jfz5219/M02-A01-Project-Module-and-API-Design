4ea1bd72d0bf23f63b98c3d5c2bfba674ec5639b
ejk1220 <ejkennedy137@gmail.com>
2021-11-07 18:31:28 -0500

    Added JFrame Functionallity

--- Group9WallyLandApp/src/main/java/EmployeePortal/PortalCredentials.java
+++ Group9WallyLandApp/src/main/java/EmployeePortal/PortalCredentials.java
@@ -45,15 +45,21 @@ public class PortalCredentials {
             buttonPanel.add(attractionsButton);
             buttonPanel.add(ridesButton);
             buttonPanel.add(helpButton);
+            
         JFrame accountFrame = new JFrame();
         JTextField uNameField = new JTextField();
-        uNameField.setPreferredSize(new Dimension(100,100));
+        uNameField.setPreferredSize(new Dimension(150,50));
         JTextField accountStatusField = new JTextField();
-        accountStatusField.setPreferredSize(new Dimension(100,100));
-        accountFrame.add(uNameField);
-      //  accountFrame.add(accountStatusField);
+        accountStatusField.setPreferredSize(new Dimension(150,50));
+        
+        JPanel textFieldPanel = new JPanel();
+        textFieldPanel.add(uNameField);
+        textFieldPanel.add(accountStatusField);
+        textFieldPanel.setPreferredSize(new Dimension(400,300));
         uNameField.setText("Admin");
         accountStatusField.setText("Employee");
+        accountFrame.add(textFieldPanel);
+        
         accountFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         accountFrame.setLocationRelativeTo(null);
         accountFrame.setPreferredSize(new Dimension(400,300));
